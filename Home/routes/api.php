9601c6e9996425ee693a3912043a86db2d39abdc
Zachary Zajdel <42820232+Zac-Zajdel@users.noreply.github.com>
2020-01-01 04:40:54 -0500

    Better Organized routes by learning from Laravel Docs

--- Home/routes/api.php
+++ Home/routes/api.php
@@ -1,18 +1,21 @@
 <?php
 
+use App\Contact;
 use Illuminate\Http\Request;
 
 // Grouping allows us to apply the authentication to all CRUD operations
 Route::middleware('auth:api')->group(function () {
-    Route::get('/contacts', 'ContactsController@index');
-    Route::get('/contacts/{contact}', 'ContactsController@show');
-    Route::post('/contacts', 'ContactsController@store');
-    Route::put('/contacts/{contact}', 'ContactsController@update');
-    Route::delete('/contacts/{contact}', 'ContactsController@destroy');
+    Route::prefix('contacts')->group(function () {
+        Route::get('/', 'ContactsController@index');
+        Route::get('/{contact}', 'ContactsController@show');
+        Route::post('/', 'ContactsController@store');
+        Route::put('/{contact}', 'ContactsController@update');
+        Route::delete('/{contact}', 'ContactsController@destroy');
+    });
+  
+    Route::get('/birthdays', 'BirthdaysController@index');
 
-    Route::get('birthdays', 'BirthdaysController@index');
-
-    Route::post('search', 'SearchController@index');
+    Route::post('/search', 'SearchController@index');
 
     Route::get('/homepage', 'HomeController@show');
 });
